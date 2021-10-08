538cf4672a85cb9a807bee36d5d02915612089e7
dom-the-dev <hi@domthedev.com>
2021-10-08 06:57:49 +0200

    dist

--- dist/database.js
+++ dist/database.js
@@ -6,10 +6,10 @@ exports.__esModule = true;
 var dotenv_1 = __importDefault(require("dotenv"));
 var pg_1 = require("pg");
 dotenv_1["default"].config();
-var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_DB_TEST = _a.POSTGRES_DB_TEST, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD;
+var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_DB_TEST = _a.POSTGRES_DB_TEST, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, ENV = _a.ENV;
 var client = new pg_1.Pool({
     host: POSTGRES_HOST,
-    database: process.env.ENV === 'test' ? POSTGRES_DB_TEST : POSTGRES_DB,
+    database: ENV === 'test' ? POSTGRES_DB_TEST : POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD
 });
